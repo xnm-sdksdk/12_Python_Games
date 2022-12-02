c672723866c0a7cf15a421431615b8fa886382fb
xnm-sdksdk <n.mendonca@protonmail.com>
2022-12-02 22:30:47 +0000

    Simple Madlib Done

--- Madlibs.py
+++ Madlibs.py
@@ -1,6 +1,5 @@
 # Madlibs Game in python made by Nuno Mendonça
 
-
 """
 METHODS OF CONCATENATION
 
@@ -11,5 +10,15 @@ METHODS OF CONCATENATION
 f ""{nameofvariable}
 """
 
+# string inputs
+first_word = str(input("Insert the first word: "))
 
+second_word = str(input("Insert the second word: "))
 
+third_word = str(input("Insert the third word: "))
+
+fourth_word = str(input("Insert the fourth word: "))
+
+# string concatenation
+madlib_entry = f"Python is very {first_word}! You can do a lot of {second_word} stuff with it.\
+    Programming is a very powerful {third_word} tool. Use it {fourth_word}!"
