290cda17624fcd6e2b83434f8f1de3759c2c22ad
xnm-sdksdk <n.mendonca@protonmail.com>
2022-12-03 00:31:15 +0000

    Rock paper scissors done

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -4,6 +4,9 @@
 
 import random
 
+
+# Code Logic: r > s, s > p, p > r
+
 # start function to start the game
 def play():
     # user input for him to choose is weapon
@@ -11,6 +14,23 @@ def play():
 
     # random choice returns a random choice from a sequence    
     computer = random.choice(["r", "p", "s"])
+    
+    # if you pass this first if statement, it means that the result is a win or a loss    
+    if user == computer:
+        return "I can't believe it! It's a tie!"
+
+    # if you get to this conditional then the result is a win or a loss
+    if win(user, computer):
+        return "You won! Congratulations!"
+    
+    # if you get to this conditional then the result is a loss
+    return "Good try, but you lost!"
 
 
-
+def win(user, computer):
+    # this function is going to return true if the user wins
+    if (user == "r" and computer == "s") or (user == "s" and computer == "p") or (user == "p" and computer == "r"):
+        return True
+    
+# we make this print to show the result of the game and the questions to the user
+print(play())
